ac9eb62bbb7531410c782c3982fbd0eb5bb50e5e
intellisense <oyeknamiakandeworld@gmail.com>
2020-03-02 02:23:50 +0100

    create model and controller

---
+++ src/main/java/com/intellisense/coronavirustracker/controller/MainController.java
@@ -0,0 +1,7 @@
+package com.intellisense.coronavirustracker.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class MainController {
+}
--- src/main/java/com/intellisense/coronavirustracker/service/CoronaVirusDataService.java
+++ src/main/java/com/intellisense/coronavirustracker/service/CoronaVirusDataService.java
@@ -1,17 +1,20 @@
 package com.intellisense.coronavirustracker.service;
 
 
+import com.intellisense.coronavirustracker.model.LocationStats;
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVRecord;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import sun.net.www.http.HttpClient;
 
 import javax.annotation.PostConstruct;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 public class CoronaVirusDataService {
@@ -21,9 +24,14 @@ public class CoronaVirusDataService {
 
     private static String VIRUS_DATA_URL = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/archived_data/archived_time_series/time_series_2019-ncov-Confirmed.csv";
 
+    private List<LocationStats> allStats = new ArrayList<>();
+
     // make an http call to the url
     @PostConstruct
+    @Scheduled(cron = "* * 1 * * *")
     public void fetchVirusData() throws IOException {
+
+        List<LocationStats> newStats = new ArrayList<>();
 
         URL url = new URL(VIRUS_DATA_URL);
         HttpURLConnection connection = (HttpURLConnection) url.openConnection();
@@ -36,10 +44,23 @@ public class CoronaVirusDataService {
         while ((inputLine = in.readLine()) != null){
             content.append(inputLine);
         }
-        in.close();
+        //in.close();
 
-        connection.disconnect();
+        //connection.disconnect();
 
         System.out.println(content);
+
+        StringReader reader = new StringReader(content.toString());
+        Iterable<CSVRecord> records = CSVFormat.DEFAULT.withFirstRecordAsHeader().parse(reader);
+        for (CSVRecord record : records) {
+            LocationStats locationStats = new LocationStats();
+            locationStats.setState(record.get("Province/State"));
+            locationStats.setCountry(record.get("Country/Region"));
+            locationStats.setLatestTotalCases(Integer.parseInt(record.get(record.size()-1)));
+
+            System.out.println(locationStats);
+            newStats.add(locationStats);
+        }
+        this.allStats = newStats;
     }
 }
